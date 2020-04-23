1ddea87c74a1d133186316443aa3ebf911d02d7c
Ivan Violentov <violentoff@gmail.com>
2020-04-23 13:25:39 +0300

    Add example proxy for simple endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,31 @@
+import dotenv from 'dotenv'
+import request from 'request-promise'
+
 import express, {
   Response,
   Application,
 } from 'express'
 
+dotenv.config()
+
+const ALCHEMY_BASE_URL = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMYAPI_KEY}`
+
 const app: Application = express()
 
-app.get('/', function (_, res: Response) {
-  res.send('Hello World')
+app.post('/eth_blockNumber', async function (_, res: Response) {
+  const result = await request({
+    method: 'POST',
+    uri: ALCHEMY_BASE_URL,
+    body: {
+      'jsonrpc': "2.0",
+      'method': "eth_blockNumber",
+      'params': [],
+      'id': 83,
+    },
+    json: true,
+  })
+
+  res.send(result)
 })
 
 app.listen(3000)
